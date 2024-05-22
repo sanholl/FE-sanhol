331303da7a24223838c45ee0fe9387c5515d6b5a
daeun <y0825131@naver.com>
2024-05-22 21:28:42 +0900

    feat: 랜덤 숫자 출력 함수 생성

---
+++ bulls-and-cows/src/random.js
@@ -0,0 +1,19 @@
+export function generateRandomNumber() {
+    // 1 ~ 9까지 숫자를 이용
+    const candidates = [1,2,3,4,5,6,7,8,9];
+
+    // 랜덤하게 섞어서 4자리 숫자만 이용할 예정
+    const pickedNumbers = shuffle(candidates).splice(0, 4); // 4자리를 뽑음
+
+    return pickedNumbers;
+}
+
+function shuffle(array) {
+    // Math.random(), 0 ~ 1까지 랜덤하게 뽑아주는데요
+    // array.sort()
+
+    // array.sort(() => 음수를 반환하면 내림차순, 양수를 반환하면 오름차순으로 정렬)
+    return array.sort(() => {
+        return Math.random() - 0.5 // -0.5 ~ 0.5
+    })
+}
