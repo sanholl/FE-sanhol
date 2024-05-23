35be97b3b5a2a7b409345053ef7381087cf47046
sanholl <edgar.jang93@hanmail.net>
2024-05-23 21:21:01 +0900

    feat: 유효성 검사 기능 개발

--- bulls-and-cows/src/App.js
+++ bulls-and-cows/src/App.js
@@ -16,9 +16,34 @@ function App() {
     setAnswer(event.target.value);
   }
   const handleSubmit = () => {
-    // 스트라이크, 볼, 정답 유무
     const answers = answer.split('').map(Number);
     // const answers = Array.from(answer, Number);
+
+    if(answers.some(number => isNaN(number))) {
+      return alert('숫자만 입력해주세요.');
+    }
+
+    if(answers.length !== 4) {
+      return alert('4자리 숫자만 입력해주세요.');
+    }
+
+    const isDupulicate = answers.some(number => {
+      // [1, 2, 3, 4]
+      // -> 앞에서 탐색했을때 index 0
+      // <- 뒤에서부터 탐색했을때 index 0
+      // [1, 1, 2, 4]
+      // -> 앞에서 탐색했을때 index 0
+      // <- 뒤에서부터 탐색했을때 index 1
+      // 중복의 경우 앞과 뒤에서 탐색했을때 index가 다르다는 점을 활용
+
+      // answers.indexOf(number) // 앞에서부터 배열을 탐색
+      // answers.lastIndexOf(number) // 뒤에서부터 배열을 탐색
+      return answers.indexOf(number) !== answers.lastIndexOf(number);
+    })
+
+    if(isDupulicate) {
+      return alert('입력 값에 중복이 있어요.');
+    }
 
     const { strike,ball } = randomNumber.reduce((prev, cur, index) =>{
       // 같은 자리에 같은 수가 존재하면 스트라이크
