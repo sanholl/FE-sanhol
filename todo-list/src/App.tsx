fa919816a8420b746375aeaa8661bd855f75437b
sanholl <edgar.jang93@hanmail.net>
2024-05-29 22:21:30 +0900

    feat: useState

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import TodoHeader from './Header/TodoHeader';
 import TodoInput from './Input/TodoInput';
@@ -6,11 +6,28 @@ import TodoListTools from './Tools/TodoListTools';
 import Divider from './Divider/Divider';
 import TodoList from './List/TodoList';
 
+type TodoType = {
+  id: number
+  text: string
+  isChecked: boolean
+}
+
 function App() {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleTextChange = (text:string) => {
+    setText(text);
+  }
+
+  const handleSubmit = () => {
+    console.log(text)
+  }
+
   return (
     <main className="App">
       <TodoHeader/>
-      <TodoInput/>
+      <TodoInput text={text} onTextChange={handleTextChange} onSubmit={handleSubmit}/>
       <TodoListTools/>
       <Divider/>
       <TodoList/>
--- todo-list/src/Input/TodoInput.tsx
+++ todo-list/src/Input/TodoInput.tsx
@@ -1,15 +1,32 @@
 import { RiChatNewLine } from 'react-icons/ri';
 import styles from './TodoInput.module.css';
+import { ChangeEvent, FormEvent } from 'react';
 
-const TodoInput = () => {
-    return (
-        <section className={styles.container}>
-          <form className={styles.formContainer}>
-            <input className={styles.input} placeholder={'해야할 Todo'}/>
-          </form>
-          <button className={styles.enter}><RiChatNewLine/></button>
-        </section>
-    );
+interface TodoInputProps {
+  text: string
+  onTextChange: (text:string) => void
+  onSubmit: () => void
+}
+
+const TodoInput = (props: TodoInputProps) => {
+
+  const handleInputChange = (event:ChangeEvent<HTMLInputElement>) => {
+    props.onTextChange(event.target.value);
+  }
+
+  const handleSubmit = (event:FormEvent) => {
+    event.preventDefault();
+    props.onSubmit();
+  }
+  
+  return (
+    <section className={styles.container}>
+      <form className={styles.formContainer} onSubmit={handleSubmit}>
+        <input className={styles.input} placeholder={'해야할 Todo'} value={props.text} onChange={handleInputChange}/>
+        <button type='submit' className={styles.enter}><RiChatNewLine /></button>
+      </form>
+    </section>
+  );
 }
 
 export default TodoInput;
