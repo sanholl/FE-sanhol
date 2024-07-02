b481f752a5a95349ed0da349f59136ccc0d42b6c
sanholl <edgar.jang93@hanmail.net>
2024-07-02 23:14:38 +0900

    feat: CRA 사용하지 않고 React 프로젝트 만들기

---
+++ kakao-map/scripts/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 번들링이 일어날때 css 파일을 별도의 파일로 호환하겠다는 플러그인
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  entry: './src/index.jsx',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: 'static/js/[name].[contenthash:8].js',
+    clean: true
+  },
+  devtool: isProduction ? false : 'eval-source-map',
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+    client: {
+      overlay: true,
+      progress: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader'
+            }
+          },
+          {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: [
+              isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+              'css-loader'
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [ isProduction ? new HtmlWebpackPlugin({
+    template: 'public/index.html',
+    minify: true
+  }) :
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    }),
+    isProduction ? new MiniCssExtractPlugin({
+      linkType: false,
+      filename: '[name].[contenthash:8].css'
+    }) : undefined
+  ].filter(Boolean)
+}
---
+++ kakao-map/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="react">
+      Hello React!
+    </div>
+  )
+}
+
+export default App;
