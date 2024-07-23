d25996cb7fe4a0a021070c71d262bf304965f02a
Hansol Jang <sol@bizspring.co.kr>
2024-07-24 08:44:54 +0900

    chore: app directory 수정

--- fsd-boilerplate/src/app/index.tsx
+++ fsd-boilerplate/src/app/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './app/app';
-import reportWebVitals from './reportWebVitals';
+import { App } from './app';
+import reportWebVitals from '../reportWebVitals';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
